3a15923da154cefb5bc61ba07452e43e10fd53d0
manda <manda@ws.cs.kobe-u.ac.jp>
2020-08-03 10:35:52 +0900

    初期化

---
+++ src/main/java/jp/kobe_u/cs/daikibo/tsubuyaki/ServletInitializer.java
@@ -0,0 +1,12 @@
+package jp.kobe_u.cs.daikibo.tsubuyaki;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TsubuyakiApplication.class);
+	}
+}
